78804a3e6cd869a64a1e543d041c82d2101e5833
cjlee38 <gptpem38@gmail.com>
2022-11-22 17:05:19 +0900

    feat: ManyToOne 단방향, fetch join 시 동일성 보장 확인

---
+++ spring-playground/src/main/java/study/jpa/relations/manytoone/Desk.java
@@ -0,0 +1,19 @@
+package study.jpa.relations.manytoone;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Desk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int number;
+}
