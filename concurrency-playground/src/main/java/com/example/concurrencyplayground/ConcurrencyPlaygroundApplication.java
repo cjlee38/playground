bc083207fdcba26a16cd9411af7d3a520820970b
cjlee38 <gptpem38@gmail.com>
2022-12-31 22:40:17 +0900

    chore: add concurrency-playground

---
+++ concurrency-playground/src/main/java/com/example/concurrencyplayground/ConcurrencyPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrencyplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyPlaygroundApplication.class, args);
+	}
+
+}
---
+++ concurrency-playground/src/test/java/com/example/concurrencyplayground/ConcurrencyPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrencyplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
