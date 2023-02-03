f061dc1e08630c686a5a34eb4599e9d43284846c
cjlee38 <gptpem38@gmail.com>
2023-02-03 22:43:15 +0900

    chore: aop playground 추가

---
+++ aop-playground/src/main/java/com/example/aopplayground/AopPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.aopplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopPlaygroundApplication.class, args);
+	}
+
+}
