3fc4c2db5992c78b7638bf5e012e95a0783ffe3b
chalee <gptpem38@gmail.com>
2023-02-22 09:47:40 +0900

    chore: set up batch application properties

--- spring-batch-playground/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ spring-batch-playground/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,8 +1,12 @@
 package com.example.demo
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableBatchProcessing
+@EnableJpaRepositories
 @SpringBootApplication
 class DemoApplication
 
