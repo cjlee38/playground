fbbc429b94f0216fe5b3f71fc03473fcc3fa85ce
chalee <gptpem38@gmail.com>
2023-02-26 23:57:59 +0900

    feat: string intern 확인 추가

---
+++ java-playground/src/main/java/study/java/primitives/Strings.java
@@ -0,0 +1,9 @@
+package study.java.primitives;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String str = "hello world!";
+        String intern = str.intern();
+    }
+}
