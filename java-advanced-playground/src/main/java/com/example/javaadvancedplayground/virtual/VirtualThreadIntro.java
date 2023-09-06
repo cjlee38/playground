fa67a338e54b1efc0fe90b76bedcc6b4eeffd530
cjlee38 <cjlee38@github.com>
2023-09-06 20:37:31 +0900

    feat: add simple virtual thread code

--- java-advanced-playground/src/main/java/com/example/javaadvancedplayground/virtual/VirtualThreadIntro.java
+++ java-advanced-playground/src/main/java/com/example/javaadvancedplayground/virtual/VirtualThreadIntro.java
@@ -1,7 +1,14 @@
 package com.example.javaadvancedplayground.virtual;
 
 public class VirtualThreadIntro {
-    public static void main(String[] args) {
-        Thread.yield()
+    public static void main(String[] args) throws InterruptedException {
+        Thread vThread = Thread.ofVirtual().start(() -> {
+            String name = Thread.currentThread().toString();
+            // Strig name = Thread.currentThread().getName(); // it returns ""
+            System.out.println("hello world : " + name);
+        });
+        boolean virtual = vThread.isVirtual();
+        System.out.println("virtual = " + virtual);
+        Thread.sleep(1000L);
     }
 }
