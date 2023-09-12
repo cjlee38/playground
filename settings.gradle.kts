df70694b7a23ab389f8197329b449dfd35470d46
cjlee38 <cjlee38@github.com>
2023-09-13 08:27:36 +0900

    chore: add version catalog
    not success yet

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,3 +31,60 @@ pluginManagement {
         }
     }
 }
+
+fun VersionCatalogBuilder.item(alias: String, depiction: String): String {
+    val groupArtifactVersions = depiction.split(":")
+    when (groupArtifactVersions.size) {
+        2 -> library(alias, groupArtifactVersions[0], groupArtifactVersions[1]).withoutVersion()
+        3 -> library(
+            alias,
+            "${groupArtifactVersions[0]}:${groupArtifactVersions[1]}:${groupArtifactVersions[2]}"
+        )
+    }
+    return alias
+}
+
+fun VersionCatalogBuilder.spec(alias: String, vararg items: String): Spec {
+    bundle(alias, items.toList())
+    return Spec(alias, items.toList())
+}
+
+data class Spec(
+    private val bundleAlias: String,
+    private val libAliases: List<String>
+) {
+    val items: Array<String>
+        get() = libAliases.toTypedArray()
+}
+
+/**
+ * tobe handled, see [gradle documentation](https://docs.gradle.org/8.0.2/userguide/platforms.html)
+ */
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            // springs
+            val springBootSpec = spec(
+                "springboot",
+                item("springboot-test", "org.springframework.boot:spring-boot-starter-test"),
+                item("springboot-web", "org.springframework.boot:spring-boot-starter-web"),
+                item("springboot-jpa", "org.springframework.boot:spring-boot-starter-data-jpa"),
+            )
+            // kotlins
+            val kotlinSpec = spec(
+                "kotlin",
+                item("kotlin-reflect", "org.jetbrains.kotlin:kotlin-reflect"),
+                item("kotlin-jackson", "com.fasterxml.jackson.module:jackson-module-kotlin"),
+                item("kotlin-logging", "io.github.microutils:kotlin-logging:1.7.6"),
+                item("kotest", "io.kotest:kotest-runner-junit5:5.4.2"),
+            )
+            // kotlins + springs
+            spec(
+                "kotlin-springboot",
+                *springBootSpec.items,
+                *kotlinSpec.items,
+                item("kotest-spring", "io.kotest.extensions:kotest-extensions-spring:1.1.2")
+            )
+        }
+    }
+}
