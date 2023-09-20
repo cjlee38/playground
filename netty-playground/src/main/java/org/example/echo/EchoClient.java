6d5d122cdc9f5d0179362039d02fc6a57c743ddd
cjlee38 <cjlee38@github.com>
2023-09-20 23:18:52 +0900

    feat: add echo server / client

---
+++ netty-playground/src/main/java/org/example/echo/EchoClient.java
@@ -0,0 +1,54 @@
+package org.example.echo;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import java.net.InetSocketAddress;
+import lombok.SneakyThrows;
+
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage : " + EchoClient.class.getSimpleName() + " <host> <port>");
+            return;
+        }
+
+        new EchoClient(
+                args[0],
+                Integer.parseInt(args[1])
+        ).start();
+    }
+
+    @SneakyThrows
+    public void start() {
+        EchoClientHandler handler = new EchoClientHandler();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(handler);
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+}
---
+++ netty-playground/src/main/java/org/example/echo/EchoServer.java
@@ -0,0 +1,49 @@
+package org.example.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import java.net.InetSocketAddress;
+import lombok.SneakyThrows;
+
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage : " + EchoServer.class.getSimpleName() + " <port>");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    @SneakyThrows
+    private void start() {
+        EchoServerHandler handler = new EchoServerHandler();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(handler);
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+}
